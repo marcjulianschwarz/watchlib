c2fe1418158f6c26568b4a7625e2fbfb703044dc
marcjulianschwarz <marc.julian@schwarz-interactive.de>
2021-10-12 21:50:30 +0200

    [MOD] Ecg line plot and title

--- plot.py
+++ plot.py
@@ -2,8 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-def plot_ecg(ecg_data):
-    plt.scatter(x=np.linspace(0, 1, 3000), y=ecg_data["name"][:3000], s=3)
+def plot_ecg(ecg_data, meta_data=None):
+
+    y = ecg_data["name"][:1000]
+    x = np.linspace(0, 1, len(y))[:1000]
+
+    fig, ax = plt.subplots(figsize=(10,5))
+    plt.plot(x, y)
+
+    if meta_data:
+        plt.title("Date: " + meta_data["Aufzeichnungsdatum"] + "     Classification: " + meta_data["Klassifizierung"])
+
     plt.show()
 
 
