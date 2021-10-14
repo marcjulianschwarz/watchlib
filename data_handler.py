337292b61802672ae3e32562dfbc5541da435f4e
marcjulianschwarz <marc.julian@schwarz-interactive.de>
2021-10-14 15:53:05 +0200

    [MOD] Changed types

--- data_handler.py
+++ data_handler.py
@@ -46,19 +46,20 @@ class DataLoader:
 
         return meta_data, data
 
-    def get_workout_route_root(self, route: str) -> ET.Element:
+    def __get_workout_route_root(self, route: str) -> ET.Element:
         
         tree = ET.parse(open(self.workout_path + route))
         return tree.getroot()
 
-    def read_workout_route(self, route):
+    def read_workout_route(self, route) -> pd.DataFrame:
         
-        route = self.get_workout_route_root(route)
+        route = self.__get_workout_route_root(route)
         ns = {"gpx": "http://www.topografix.com/GPX/1/1"}
         tracks = route.findall('gpx:trk', ns)
 
         data = {
-            "point": [],
+            "lon": [],
+            "lat": [],
             "time": [],
             "elevation": [],
             "speed": [],
@@ -71,28 +72,29 @@ class DataLoader:
             track_segments = track.findall('gpx:trkseg', ns)
             for track_segment in track_segments:
                 track_points = track_segment.findall('gpx:trkpt', ns)
+                for track_point in track_points:
+                    
+                    elevation = track_point.findall('gpx:ele', ns)[0].text
+                    time = track_point.findall('gpx:time', ns)[0].text
+                    extension = track_point.findall('gpx:extensions', ns)[0]
 
-                elevation = [track_point.findall('gpx:ele', ns)[0].text for track_point in track_points]
-                time = [track_point.findall('gpx:time', ns)[0].text for track_point in track_points]
-
-                data["elevation"].append(elevation)
-                data["time"].append(time)
-
-                extensions = [track_point.findall('gpx:extensions', ns)[0] for track_point in track_points]
-
-                for extension in extensions:
+                    lon = track_point.get("lon")
+                    lat = track_point.get("lat")
                     speed = extension.findall('gpx:speed', ns)[0].text
                     course = extension.findall('gpx:course', ns)[0].text
                     hAcc = extension.findall('gpx:hAcc', ns)[0].text
                     vAcc = extension.findall('gpx:vAcc', ns)[0].text
 
-                    data["speed"].append(speed)
-                    data["course"].append(course)
-                    data["hAcc"].append(hAcc)
-                    data["vAcc"].append(vAcc)
+                    data["lon"].append(float(lon))
+                    data["lat"].append(float(lat))
+                    data["elevation"].append(float(elevation))
+                    data["time"].append(time)
+                    data["speed"].append(float(speed))
+                    data["course"].append(float(course))
+                    data["hAcc"].append(float(hAcc))
+                    data["vAcc"].append(float(vAcc))
 
-
-        return data
+        return pd.DataFrame(data)
 
     def get_routes(self):
 
