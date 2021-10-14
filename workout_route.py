cbbfb28f32ed710bee883fdd104fb4d515ed3dd3
marcjulianschwarz <marc.julian@schwarz-interactive.de>
2021-10-14 15:53:50 +0200

    [ADD] Init workout_routes.py

---
+++ workout_route.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def project_to_xy(lon, lat):
+
+    middle_of_map_lat = np.mean(lat)
+    lon = lon*np.abs(np.cos(middle_of_map_lat))
+    return lon, lat
+
+
+def workout_three_d(workout_route, resolution=0.5):
+
+    title = pd.to_datetime(workout_route["time"].iloc[0]).date()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x, y = project_to_xy(workout_route["lon"], workout_route["lat"])
+    elevation =  workout_route["elevation"]
+
+    strip = int(1 / resolution)
+
+    ax.plot(x[::strip], y[::strip], elevation[::strip])
+    ax.set_zlim(300, 500)
+    plt.title(title)
+    plt.show()
