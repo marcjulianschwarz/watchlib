9fbbb5600636de019033a1490f1d4f608f2273c9
marcjulianschwarz <marc.julian@schwarz-interactive.de>
2021-10-15 16:20:53 +0200

    [ADD] Init ecg.py

--- ecg.py
+++ ecg.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 def plot_ecg(ecg_data, meta_data=None):
 
@@ -14,7 +13,3 @@ def plot_ecg(ecg_data, meta_data=None):
         plt.title("Date: " + meta_data["Aufzeichnungsdatum"] + "     Classification: " + meta_data["Klassifizierung"])
 
     plt.show()
-
-
-def box(data):
-    sns.boxplot(np.array(data["HKQuantityTypeIdentifierHeartRate"]).astype("float"))
