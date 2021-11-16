7c9b6e8b3132a806ffbdde23a98a00d93073591b
marcjulianschwarz <marc.julian@schwarz-interactive.de>
2021-11-16 22:17:35 +0100

    [MOD] WorkoutRoute properties and remove bpm

--- watchlib/utils/structs.py
+++ watchlib/utils/structs.py
@@ -24,54 +24,12 @@ class ECG:
 
         return data, meta_data
     
+    def __getitem__(self, key):
+        return self.data[key]
 
-    def bpm(self, a: float = 50, d: float = 180, r:float = 3, plot: bool = False) -> int:
-        """
-            Calculates heart rate (bpm) from ecg data.
-
-            a: heart beat slope amplitude threshold
-            d: heart beat distance threshold
-            r: slope calculation resolution
-            plot: plot ecg, slope and heart beat points
-        """
-        
-        x = [xx for xx in range(0, len(self.data["name"]))]
-        y = self.data["name"]
-
-        def slope(p1, p2):
-            x1, y1 = p1
-            x2, y2 = p2
-            slope = (y2 - y1)/(x2 - x1)
-            return slope
-
-        slopes = []
-        for xx in x:
-            if xx + r < len(y):
-                point1 = (xx, y.iloc[xx])
-                point2 = (xx+r, y.iloc[xx+r])
-                slopes.append(slope(point1, point2))
-
-        bpm_points = [-d]
-        for idx, slope in enumerate(slopes):
-            if np.abs(slope) > a and np.abs(bpm_points[-1] - x[idx]) > d:
-                bpm_points.append(x[idx])
-        bpm_points = bpm_points[1:]
-
-        bp = len(bpm_points)*2
-        #bp_val = valid[name]
-        #error = np.abs(bp - bp_val)
-    
-        if plot:
-            fig, ax = plt.subplots(figsize=(20, 5))
-            ax.plot(x, y, label="ECG", zorder=0)
-            ax.plot(slopes, label="slope", zorder=1)
-            ax.scatter(x=bpm_points, y=[200 for y in range(len(bpm_points))], c="r", s=60, label="heartbeat", zorder=2)
-            ax.legend()
-            plt.show()
-            return bp, fig
-
-        return bp
-
+class ECGWave:
+    def __init__(self):
+        pass
 
 class WorkoutRoute:
 
@@ -81,18 +39,55 @@ class WorkoutRoute:
     def __init__(self, data, name: str):
         if isinstance(data, pd.DataFrame):
             self.route = data
+        elif isinstance(data, ET.Element):
+            self.route = self.__read_route(data)
         else:
-            self.route = self.__read_route()
+            raise ValueError("This workout type does not exist")
         
         self.name = name
 
     def __getitem__(self, key):
         return self.route[key]
 
-    def __read_route(self) -> pd.DataFrame:
+    @property
+    def lon(self):
+        return self.route["lon"]
+    
+    @property
+    def lat(self):
+        return self.route["lat"]
+    
+    @property
+    def time(self):
+        return self.route["time"]
+    
+    @property
+    def elevation(self):
+        return self.route["elevation"]
+    
+    @property
+    def speed(self):
+        return self.route["speed"]
+    
+    @property
+    def course(self):
+        return self.route["course"]
+
+    @property
+    def hAcc(self):
+        return self.route["hAcc"]
+    
+    @property
+    def vAcc(self):
+        return self.route["vAcc"]
+    
+    
+    
+
+    def __read_route(self, data: ET.Element) -> pd.DataFrame:
 
         ns = {"gpx": "http://www.topografix.com/GPX/1/1"}
-        tracks = self.data.findall('gpx:trk', ns)
+        tracks = data.findall('gpx:trk', ns)
 
         data = {
             "lon": [],
