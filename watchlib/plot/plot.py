2810730f0c1ca4474684d02c21a03164021c9a5b
marcjulianschwarz <info@marc-julian.de>
2022-02-09 10:08:49 +0100

    [ADD] better ecg plotting

--- watchlib/plot/plot.py
+++ watchlib/plot/plot.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from watchlib.analysis.analysis_utils import slopes_for, slopes_of_slopes_for
 from watchlib.analysis.analysis_ecg_utils import bpm_points, split_between_heartbeats
 from watchlib.utils import WorkoutRoute, ECG
-from watchlib.analysis import heart_rate_variability_pairwise
+from watchlib.analysis import heart_rate_variability_pairwise, heart_rate_variability
 
 
 def plot_elevation(route: WorkoutRoute):
@@ -24,25 +24,28 @@ def plot_side(route: WorkoutRoute):
     raise NotImplementedError
 
 
-def plot_hrv(ecg: ECG):
-    splits = split_between_heartbeats(ecg)
-    for s in splits:
-        x = [xx for xx in range(0, len(s["name"]))]
-        y = s["name"]
-        plt.plot(x, y)
-    plt.title(
-        f"ECG: {ecg.name} --- HRV: {round(heart_rate_variability_pairwise(ecg),2)}")
+def plot_ecg(ecg: ECG, _from=None, _to=None, figsize=(20, 5), c="b", title=None, return_fig=False):
+    if _from is None: _from = 0
+    if _to is None: _to = len(ecg.x)
+    if title is None: title = ecg.name
+
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.plot(ecg.x[_from:_to], ecg.y[_from:_to], c=c)
+    plt.title(title)
     plt.show()
 
+    if return_fig:
+        return fig
 
-def plot_ecg(ecg: ECG):
+
+def plot_ecg_with_slopes(ecg: ECG, figsize=(20,5)):
 
     x, y = ecg.x, ecg.y
     s = slopes_for(list(zip(x, y)))
     slopes_of_s = slopes_of_slopes_for(list(zip(x, y)))
     points = bpm_points(ecg, slopes=s)
 
-    fig, ax = plt.subplots(figsize=(20, 5))
+    fig, ax = plt.subplots(figsize=figsize)
     ax.plot(x, y, label="ECG", zorder=0)
     ax.plot(s, label="slope", zorder=1)
     ax.plot(slopes_of_s, label="slope of slopes", zorder=1)
@@ -52,3 +55,13 @@ def plot_ecg(ecg: ECG):
     ax.legend()
     plt.show()
     return fig
+
+
+def plot_ecg_hrvs_overlay(ecg: ECG, figsize=(10,5)):
+    splits = split_between_heartbeats(ecg)
+    fig, ax = plt.subplots(figsize=figsize)
+    for split in splits:
+        ax.plot([i for i in range(len(split))], split)    
+    plt.title(
+        f"ECG: {ecg.name} --- HRV: {round(heart_rate_variability_pairwise(ecg),2)}")
+    plt.show()
