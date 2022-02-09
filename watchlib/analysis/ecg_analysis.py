6a7b14664049af6b1295f4c47dd9a8ed2ab11710
marcjulianschwarz <info@marc-julian.de>
2022-02-09 10:10:05 +0100

    [ADD] ANN query for ECGs (with PCA)

--- watchlib/analysis/ecg_analysis.py
+++ watchlib/analysis/ecg_analysis.py
@@ -4,6 +4,10 @@ from typing import List
 
 from watchlib.analysis.analysis_ecg_utils import bpm_points, hrvs, hrvs_pairwise
 
+from sklearn.decomposition import PCA
+from annoy import AnnoyIndex
+
+
 
 def bpm(ecg: ECG, a: float = 50, d: float = 180, r: float = 3, sample_rate: float = 512, plot: bool = False) -> int:
     """
@@ -50,3 +54,44 @@ def ecg_waves(split) -> List[ECGWave]:
 
 def interpret_ecg_wave(ecg_wave: ECGWave):
     raise NotImplementedError
+
+
+def annoy_with_pca(ecgs: List[ECG], trees=10000) -> AnnoyIndex:
+
+    
+    min_features = min([len(ecg.y) for ecg in ecgs])
+    components = min(len(ecgs), min_features)
+
+    ecg_data = [ecg.y[:min_features] for ecg in ecgs]
+
+    pca = PCA(n_components=components)
+    pca.fit(ecg_data) 
+
+    ecgs_transformed = pca.transform(ecg_data)
+
+    t = AnnoyIndex(components, metric="angular")
+    for i, ecg in enumerate(ecgs_transformed):
+        t.add_item(i, ecg)
+    t.build(trees)
+
+    return t
+
+
+def annoy(ecgs: List[ECG], _from=None, _to=None, trees=10000) -> AnnoyIndex:
+
+    min_features = min([len(ecg.y) for ecg in ecgs])
+    
+    if _from is None: _from = 0
+    if _to is None: _to = min_features
+    features = _to - _from
+
+    ecg_data = [ecg.y[_from:_to] for ecg in ecgs]
+
+    t = AnnoyIndex(features, metric="angular")
+    for i, ecg in enumerate(ecg_data):
+        t.add_item(i, ecg)
+    t.build(trees)
+
+    return t
+
+
